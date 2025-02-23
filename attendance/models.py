7756111b8f04eb0e9665b648206ab1f3ea6236c0
Roshan152002 <madleroshan358@gmail.com>
2025-02-23 08:50:05 +0530

    added model for user

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    USER_TYPE = [
+        ('student','student'),
+        ('teacher','teacher'),
+    ]
+    user_type = models.CharField(max_length=10,choices = USER_TYPE)
+
+    def __str__(self):
+        return f'{self.username} ({self.user_type})'
