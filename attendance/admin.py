0c90e6381cf4794c9872f7553e0007bf2d20849d
Madle Roshan <132247612+Roshan152002@users.noreply.github.com>
2025-03-07 18:45:28 +0530

    added the base html file

--- attendance/admin.py
+++ attendance/admin.py
@@ -5,8 +5,8 @@ from .models import *
 admin.site.register(User)
 admin.site.register(Student)
 admin.site.register(Teacher)
-admin.site.register(Subject)
-admin.site.register(Attendance)
-admin.site.register(Exam)
-admin.site.register(ExamSubmission)
+# admin.site.register(Subject)
+# admin.site.register(Attendance)
+# admin.site.register(Exam)
+# admin.site.register(ExamSubmission)
 
